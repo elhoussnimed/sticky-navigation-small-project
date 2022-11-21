190cf17f53194f503272351009d049cc206e7716
mohammed el houssni <91427616+elhoussnimed@users.noreply.github.com>
2022-11-21 18:11:44 +0100

    add files to github

---
+++ app.js
@@ -0,0 +1,61 @@
+const header = document.querySelector("header");
+const heroSection = document.querySelector(".hero");
+const links = document.querySelectorAll("header nav ul li");
+const nav = document.querySelector("header nav ul");
+const openNav = document.querySelector("header .hamburger");
+const closeNav = document.querySelector("header .close");
+
+// add style to header when scrollin
+
+window.addEventListener("scroll", fixedNavigationStyle);
+
+function fixedNavigationStyle() {
+  const heroHeight = heroSection.clientHeight;
+  if (scrollY >= heroHeight / 3) {
+    header.classList.add("fixed");
+  } else {
+    header.classList.remove("fixed");
+  }
+}
+
+// change active link
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    links.forEach((link) => link.classList.remove("active"));
+    link.classList.add("active");
+  });
+});
+
+// show and hide phone nav menu
+
+openNav.addEventListener("click", () => {
+  showmobileNav();
+  changeMobileNavIcone();
+});
+closeNav.addEventListener("click", () => {
+  hidemobileNav();
+  changeMobileNavIcone();
+});
+
+function showmobileNav() {
+  nav.classList.add("show");
+  if (header.classList.contains("fixed")) {
+    nav.style.cssText = "top: 135%; background-color: white;";
+  } else {
+    nav.style.cssText = "top: 100%; background-color: #272524";
+  }
+}
+
+function hidemobileNav() {
+  nav.classList.remove("show");
+}
+
+function changeMobileNavIcone() {
+  if (nav.classList.contains("show")) {
+    openNav.style.display = "none";
+    closeNav.style.display = "block";
+  } else {
+    openNav.style.display = "block";
+    closeNav.style.display = "none";
+  }
+}
